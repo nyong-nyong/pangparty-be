85a0a81004529cb8f0c2a8953cba6d29d8c9190d
teamzamong <teamzamong@gmail.com>
2023-01-24 21:06:58 +0900

    S08P12A306-302 init: generate spring boot project

---
+++ src/main/java/nyongnyong/pangparty/PangpartyApplication.java
@@ -0,0 +1,13 @@
+package nyongnyong.pangparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PangpartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PangpartyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nyongnyong/pangparty/PangpartyApplicationTests.java
@@ -0,0 +1,13 @@
+package nyongnyong.pangparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PangpartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
