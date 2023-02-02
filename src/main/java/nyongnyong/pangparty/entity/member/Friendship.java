47ebb7b4084ccaf664c7513625f95f51acd6a75c
randallkk <randallkk.sw@gmail.com>
2023-02-02 09:26:05 +0900

    S08P12A306-306 fix: import 수정

--- src/main/java/nyongnyong/pangparty/entity/member/Friendship.java
+++ src/main/java/nyongnyong/pangparty/entity/member/Friendship.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
