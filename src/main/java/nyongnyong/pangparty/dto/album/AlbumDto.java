6337848d0d64985acdf03f9b19d979a5da3d1953
randallkk <randallkk.sw@gmail.com>
2023-02-06 17:37:09 +0900

    S08P12A306-581 feat: Album domain DTO 작성

--- src/main/java/nyongnyong/pangparty/dto/album/AlbumDto.java
+++ src/main/java/nyongnyong/pangparty/dto/album/AlbumDto.java
@@ -1,12 +1,12 @@
 package nyongnyong.pangparty.dto.album;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
+import nyongnyong.pangparty.entity.album.Album;
 
 import javax.persistence.Id;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -15,7 +15,22 @@ public class AlbumDto {
 
     @Id
     private Long uid;
-    private String eventUid;
-    private List<String> albumMediaUid;
+    private Long eventUid;
+    @Builder.Default
+    private List<Long> albumMediaUid = new ArrayList<>();
+
+    @Builder
+    public AlbumDto(Long uid, Long eventUid, List<Long> albumMediaUid) {
+        this.uid = uid;
+        this.eventUid = eventUid;
+        this.albumMediaUid = albumMediaUid;
+    }
+
+    // Entity -> Dto
+    public AlbumDto(Album album) {
+        this.uid = album.getUid();
+        this.eventUid = album.getEvent().getUid();
+        this.albumMediaUid = album.getAlbumMedia().stream().map(albumMedia -> albumMedia.getUid()).collect(Collectors.toList());
+    }
 
 }
--- src/main/java/nyongnyong/pangparty/dto/album/AlbumMediaDto.java
+++ src/main/java/nyongnyong/pangparty/dto/album/AlbumMediaDto.java
@@ -4,12 +4,15 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import nyongnyong.pangparty.entity.album.AlbumMedia;
+import org.hibernate.validator.constraints.URL;
 import org.hibernate.validator.constraints.UniqueElements;
 
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -18,17 +21,32 @@ public class AlbumMediaDto {
 
     @Id
     private Long uid;
-    @NotNull
+    @NotNull(message = "앨범 UID는 필수입니다.")
     private Long albumUid;
-    @UniqueElements
+    @UniqueElements(message = "중복된 댓글 UID가 있습니다.")
     private List<Long> albumMediaCommentsUid;
-    @UniqueElements
+    @UniqueElements(message = "중복된 좋아요 UID가 있습니다.")
     private List<Long> albumMediaLikesUid;
-    private Long mediaUid;
+    @URL(message = "URL 형식이 아닙니다.")
+    private String mediaUrl;
     private String extension;
     private LocalDateTime uploadTime;
     private LocalDateTime takenTime;
     private String takenLat;
     private String takenLng;
+
+    // Entity -> Dto
+    public AlbumMediaDto(AlbumMedia albumMedia) {
+        this.uid = albumMedia.getUid();
+        this.albumUid = albumMedia.getAlbum().getUid();
+        this.albumMediaCommentsUid = albumMedia.getAlbumMediaComments().stream().map(albumMediaComment -> albumMediaComment.getUid()).collect(Collectors.toList());
+        this.albumMediaLikesUid = albumMedia.getAlbumMediaLikes().stream().map(albumMediaLike -> albumMediaLike.getUid()).collect(Collectors.toList());
+        this.mediaUrl = albumMedia.getMediaUrl();
+        this.extension = albumMedia.getExtension();
+        this.uploadTime = albumMedia.getUploadTime();
+        this.takenTime = albumMedia.getTakenTime();
+        this.takenLat = albumMedia.getTakenLat();
+        this.takenLng = albumMedia.getTakenLng();
+    }
 
 }
