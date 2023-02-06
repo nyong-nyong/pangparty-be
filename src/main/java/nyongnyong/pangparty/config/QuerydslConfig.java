908476fd93c90ae0f46c108ab94ca9111049af21
randallkk <randallkk.sw@gmail.com>
2023-02-07 03:26:32 +0900

    S08P12A306-581 chore: Querydsl 추가

---
+++ src/main/java/nyongnyong/pangparty/config/QuerydslConfig.java
@@ -0,0 +1,18 @@
+package nyongnyong.pangparty.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+public class QuerydslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(){
+        return new JPAQueryFactory(entityManager);
+    }
+}
