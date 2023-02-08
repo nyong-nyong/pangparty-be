954d398cc2de2c2a520870f316d19dbf4a620d9b
heeheejj <kjyk0835@naver.com>
2023-02-08 22:32:27 +0900

    S08P12A306-584 fix: 이벤트 소개 페이지 조회 api 완료

--- src/main/java/nyongnyong/pangparty/dto/event/EventIntroduceRes.java
+++ src/main/java/nyongnyong/pangparty/dto/event/EventIntroduceRes.java
@@ -20,19 +20,19 @@ public class EventIntroduceRes {
     private boolean isLiked;    // eventUid -> 이벤트 좋아요 테이블(event_like)
     private String introduction;
     private String imgUrl;
-    private List<EventHashtag> hashtags;    // eventUid -> 이벤트 해시태그 테이블(event_hashtag) -> 해시태그(hashtag) -> 해시태그명(hashtag.name)
-    private List<AlbumMedia> albumMedias; // eventUid -> 이벤트 앨범 테이블(album) -> 앨범 미디어(album_media) -> 미디어URL(album_media.media_url)
+    private List<String> hashtagNames;    // eventUid -> 이벤트 해시태그 테이블(event_hashtag) -> 해시태그(hashtag) -> 해시태그명(hashtag.name)
+//    private List<String> albumMediaUrls; // eventUid -> 이벤트 앨범 테이블(album) -> 앨범 미디어(album_media) -> 미디어URL(album_media.media_url) // 근데 이렇게하면 "name": "url1", "name": "url2", 이런식이 아니라 ["url1", "url2"] 이렇게 주게됨
 
     @Builder
     public EventIntroduceRes(String targetId, LocalDate dDay, String eventName, boolean isLiked,
-                             String introduction, String imgUrl, List<EventHashtag> hashtags, List<AlbumMedia> albumMedias) {
+                             String introduction, String imgUrl, List<String> hashtagNames) {
         this.targetId = targetId;
         this.dDay = dDay;
         this.eventName = eventName;
         this.isLiked = isLiked;
         this.introduction = introduction;
         this.imgUrl = imgUrl;
-        this.hashtags = hashtags;
-        this.albumMedias = albumMedias;
+        this.hashtagNames = hashtagNames;
+//        this.albumMediaUrls = albumMediaUrls;
     }
 }
--- src/main/java/nyongnyong/pangparty/repository/event/EventRepositoryImpl.java
+++ src/main/java/nyongnyong/pangparty/repository/event/EventRepositoryImpl.java
@@ -6,10 +6,12 @@ import nyongnyong.pangparty.dto.event.EventIntroduceRes;
 import nyongnyong.pangparty.entity.album.AlbumMedia;
 import nyongnyong.pangparty.entity.event.Event;
 import nyongnyong.pangparty.entity.event.QEvent;
+import nyongnyong.pangparty.entity.event.QEventHashtag;
 import nyongnyong.pangparty.entity.event.QEventLike;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -21,8 +23,9 @@ public class EventRepositoryImpl implements EventRepositoryCustom{
     public EventIntroduceRes findEventIntroduceByEventUid(Long memberUid, Long eventUid) {
         QEvent qEvent = QEvent.event;
         QEventLike eventLike = QEventLike.eventLike;
+        QEventHashtag eventHashtag = QEventHashtag.eventHashtag;
 
-        Long tempIsLiked = queryFactory.select(qEvent.count())
+        Integer tempIsLiked = queryFactory.selectOne()      // select(qEvent.count()) 대신에 selectOne() 사용, count보다 성능이 빠름! 조건에 해당하는 row 1개만 찾으면 종료하기 때문 (exist라고 하는 듯)
                 .from(qEvent)
                 .leftJoin(eventLike)
                 .on(eventLike.event.uid.eq(qEvent.uid))
@@ -35,21 +38,29 @@ public class EventRepositoryImpl implements EventRepositoryCustom{
                 .where(qEvent.uid.eq(eventUid))
                 .fetchOne();
 
-        List<AlbumMedia> albumMedias = queryFactory.select(qEvent.album.albumMedia.any())
-                .from(qEvent)
-                .where(qEvent.uid.eq(eventUid))
-                .fetch();
+//        List<String> hashtagNames = queryFactory.select(qEvent.eventHashtags.any().hashtag.name)
+//                .from(qEvent)
+//                .where(qEvent.uid.eq(eventUid).and(qEvent.eventHashtags.any().event.uid.eq(eventUid)))
+//                .fetch();
+//
+//        List<String> albumMediaUrls = queryFactory.select(qEvent.album.albumMedia.any().mediaUrl)
+//                .from(qEvent)
+//                .where(qEvent.uid.eq(eventUid))
+//                .fetch();
 
-        System.out.println(albumMedias);
+//        System.out.println(albumMediaUrls);
+
+        List<String> hashtagNames = event.getEventHashtags().stream().map(eventHashtag1 -> eventHashtag1.getHashtag().getName()).collect(Collectors.toList());
+//        List<String> albumMediaUrls = event.getAlbum().getAlbumMedia().stream().map(AlbumMedia::getMediaUrl).collect(Collectors.toList());
 
         boolean isLiked = true;
-        if(tempIsLiked == 0) {
+        if(tempIsLiked == null || tempIsLiked == 0){
             isLiked = false;
         }
 
         EventIntroduceRes eventIntroduceRes = new EventIntroduceRes(event.getEventTarget().getTargetMember().getMemberProfile().getId()
                 , event.getDDay(), event.getEventName(), isLiked, event.getIntroduction(), event.getImgUrl(),
-                event.getEventHashtags(), event.getAlbum().getAlbumMedia());
+                hashtagNames);
 
 
         return eventIntroduceRes;
