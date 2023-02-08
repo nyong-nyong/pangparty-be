e344e49edf8a4e0407edff9db2f1b984e4377ab5
randallkk <randallkk.sw@gmail.com>
2023-02-08 17:59:38 +0900

    S08P12A306-581 feat: media service 생성

--- src/main/java/nyongnyong/pangparty/service/album/MediaServiceImpl.java
+++ src/main/java/nyongnyong/pangparty/service/album/MediaServiceImpl.java
@@ -1,4 +1,35 @@
 package nyongnyong.pangparty.service.album;
 
-public class MediaServiceImpl {
+import lombok.extern.slf4j.Slf4j;
+import nyongnyong.pangparty.entity.album.AlbumMedia;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@Service
+public class MediaServiceImpl implements MediaService{
+    @Override
+    public AlbumMedia saveMedia(MultipartFile file) {
+        return null;
+    }
+
+    @Override
+    public void deleteMedia(Long albumMediaUid) {
+
+    }
+
+    @Override
+    public MultipartFile resizeMediaToSmall(MultipartFile file) {
+        return null;
+    }
+
+    @Override
+    public MultipartFile resizeMediaToThumbnail(MultipartFile file) {
+        return null;
+    }
+
+    @Override
+    public MultipartFile reformatMedia(MultipartFile file) {
+        return null;
+    }
 }
