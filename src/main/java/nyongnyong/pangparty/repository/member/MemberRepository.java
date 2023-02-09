5d486a68a077223528819878669d698d9356e94d
heeheejj <kjyk0835@naver.com>
2023-02-10 01:03:56 +0900

    S08P12A306-584 fix: JPA For queries with named parameters 이슈 해결 - EventRepository의 @Query에서 파라미터를 넣어줄 때 @Param Annotation 추가

--- src/main/java/nyongnyong/pangparty/repository/member/MemberRepository.java
+++ src/main/java/nyongnyong/pangparty/repository/member/MemberRepository.java
@@ -3,14 +3,15 @@ package nyongnyong.pangparty.repository.member;
 import nyongnyong.pangparty.entity.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query("select m.uid from Member m left join MemberProfile mp where mp.id = :id")
     Long findUidById(String id);
 
     @Query("select m from Member m left join fetch m.memberProfile mp where mp.id = :id")
-    Member findMemberById(String id);
+    Member findMemberById(@Param("id") String id);
 
     @Query("select m from Member m where m.uid = :uid")
-    Member findMemberByUid(Long uid);
+    Member findMemberByUid(@Param("uid") Long uid);
 }
