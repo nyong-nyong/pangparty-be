229243b5074b31ff94a7146241aa087b0a42694c
heeheejj <kjyk0835@naver.com>
2023-02-10 22:42:38 +0900

    S08P12A306-584 fix: 이벤트 생성 api - RequestBody에 해시태그도 nested json으로 받고 해시태그 테이블, 이벤트해시태그 테이블에도 등록되도록 수정 및 postman 테스트 완료

--- src/main/java/nyongnyong/pangparty/repository/hashtag/HashtagRepository.java
+++ src/main/java/nyongnyong/pangparty/repository/hashtag/HashtagRepository.java
@@ -2,11 +2,14 @@ package nyongnyong.pangparty.repository.hashtag;
 
 import nyongnyong.pangparty.entity.hashtag.Hashtag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
-    Hashtag findHashtagByHashtagName(String hashtagName);
+    @Query("select h from Hashtag h where h.name = ?1")
+    Hashtag findHashtagByHashtagName(@Param(value = "name") String hashtagName);
 //    Hashtag findHashtagByHashtagName(String hashtagName);
 
 //    boolean isExistHashtagByHashtagName(String hashtagName);
