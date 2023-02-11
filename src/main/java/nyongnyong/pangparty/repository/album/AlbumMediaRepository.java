352fbfe922cfb227c69dccdd1a15f05d1e816c4e
randallkk <randallkk.sw@gmail.com>
2023-02-12 02:29:01 +0900

    S08P12A306-581 fix: optimize imports

--- src/main/java/nyongnyong/pangparty/repository/album/AlbumMediaRepository.java
+++ src/main/java/nyongnyong/pangparty/repository/album/AlbumMediaRepository.java
@@ -1,6 +1,5 @@
 package nyongnyong.pangparty.repository.album;
 
-import nyongnyong.pangparty.dto.album.AlbumMediaDetailRes;
 import nyongnyong.pangparty.entity.album.AlbumMedia;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
