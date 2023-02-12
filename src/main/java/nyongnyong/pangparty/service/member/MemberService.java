3d8bc52b84db904f1e107b49c77f09703e104f18
teamzamong <teamzamong@gmail.com>
2023-02-12 16:05:24 +0900

    S08P12A306-604 feat: Member Profile 수정 API 추가

--- src/main/java/nyongnyong/pangparty/service/member/MemberService.java
+++ src/main/java/nyongnyong/pangparty/service/member/MemberService.java
@@ -1,9 +1,12 @@
 package nyongnyong.pangparty.service.member;
 
+import nyongnyong.pangparty.dto.member.MemberProfileReq;
 import nyongnyong.pangparty.dto.member.MemberProfileRes;
 
 public interface MemberService {
 //    boolean isExistMemberByMemberId(String memberId);
 
     MemberProfileRes findMemberProfile(String memberId, String targetId);
+
+    void updateMemberProfile(String memberId, MemberProfileReq memberProfileReq);
 }
