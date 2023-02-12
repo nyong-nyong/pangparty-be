b874d9023cea637a4aebb7e2953b82687d9512a3
randallkk <randallkk.sw@gmail.com>
2023-02-12 18:28:34 +0900

    S08P12A306-583 feat: S3 연결 설정과 객체 upload, delete service

--- src/main/java/nyongnyong/pangparty/service/album/MediaService.java
+++ src/main/java/nyongnyong/pangparty/service/album/MediaService.java
@@ -1,21 +1,46 @@
 package nyongnyong.pangparty.service.album;
 
-import nyongnyong.pangparty.dto.album.AlbumMediaDetailRes;
+import nyongnyong.pangparty.common.MediaSizeType;
 import nyongnyong.pangparty.entity.album.AlbumMedia;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-
 public interface MediaService {
 
-    public AlbumMedia saveMedia(Long albumUid, Long memberUid, MultipartFile file);
+    /**
+     * S3에 미디어 업로드
+     * @param file
+     * @param key
+     * @param contentType
+     * @param contentLength
+     * @return
+     */
+    public String uploadMedia(MultipartFile file, String key, String contentType, long contentLength);
 
-    public void deleteMedia(Long albumMediaUid);
+    /**
+     * S3에서 미디어 삭제
+     * @param key
+     */
+    public void deleteMedia(String key);
 
-    public MultipartFile resizeMediaToSmall(MultipartFile file);
+    /**
+     * 미디어 앨범 상세 크기로 리사이징
+     * @param file
+     * @return
+     */
+    public MultipartFile resizeMediaToAlbumSize(MultipartFile file);
 
+    /**
+     * 미디어 thumbnail 크기로 리사이징
+     * @param file
+     * @return
+     */
     public MultipartFile resizeMediaToThumbnail(MultipartFile file);
 
+    /**
+     * 미디어 webp로 변환
+     * @param file
+     * @return
+     */
     public MultipartFile reformatMedia(MultipartFile file);
 
 }
