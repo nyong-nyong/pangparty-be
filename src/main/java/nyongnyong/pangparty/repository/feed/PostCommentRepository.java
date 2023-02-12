644990dba11c8186c4a61cda703c4ad9cf8c5b65
teamzamong <teamzamong@gmail.com>
2023-02-12 17:40:14 +0900

    S08P12A306-605 feat: 게시물 댓글 CRD 기능 추가

--- src/main/java/nyongnyong/pangparty/repository/feed/PostCommentRepository.java
+++ src/main/java/nyongnyong/pangparty/repository/feed/PostCommentRepository.java
@@ -1,7 +1,22 @@
 package nyongnyong.pangparty.repository.feed;
 
+import nyongnyong.pangparty.dto.feed.PostCommentRes;
 import nyongnyong.pangparty.entity.feed.PostComment;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
 
 public interface PostCommentRepository extends JpaRepository<PostComment, Long> {
+
+
+    @Query("select new nyongnyong.pangparty.dto.feed.PostCommentRes(pc.uid, pc.member.memberProfile.id, pc.content, pc.createTime) " +
+            "from PostComment pc where pc.post.uid = :postUid " +
+            "order by pc.createTime desc")
+    Page<PostCommentRes> findAllByPostUid(Long postUid, Pageable pageable);
+
+    @Query("select pc from PostComment pc where pc.uid = :commentUid")
+    Optional<PostComment> findByUid(Long commentUid);
 }
--- src/main/java/nyongnyong/pangparty/repository/feed/PostRepository.java
+++ src/main/java/nyongnyong/pangparty/repository/feed/PostRepository.java
@@ -2,6 +2,12 @@ package nyongnyong.pangparty.repository.feed;
 
 import nyongnyong.pangparty.entity.feed.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    boolean existsByUid(Long uid);
+
+    @Query("select p from Post p where p.uid = :postUid")
+    Post findByUid(Long postUid);
 }
