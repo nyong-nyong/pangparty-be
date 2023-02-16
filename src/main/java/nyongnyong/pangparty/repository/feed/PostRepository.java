e02f970e85a9b51303784bb2c1b9619beb54aa8e
randallkk <randallkk.sw@gmail.com>
2023-02-16 23:53:21 +0900

    S08P12A306-584 fix: @Param 전부 다 달아줌

--- src/main/java/nyongnyong/pangparty/repository/feed/PostRepository.java
+++ src/main/java/nyongnyong/pangparty/repository/feed/PostRepository.java
@@ -20,7 +20,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Query("select new nyongnyong.pangparty.dto.feed.PostRes(p.uid, p.event.uid, p.member.memberProfile.id, p.member.memberProfile.imgUrl, p.title, p.content, " +
             "p.imgUrl, p.createTime, p.modifyTime) from Post p where p.uid = :postUid")
-    PostRes findPostResByUid(@Param("uid") Long postUid);
+    PostRes findPostResByUid(@Param("postUid") Long postUid);
 
     @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query("update Post p set p.event = :event, p.title = :title, p.content = :content, p.imgUrl = :imgUrl where p.uid = :postUid")
